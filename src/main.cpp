5b68ef05488f22e9678b6ab4b9335de918203973
Rahul Senna <rahulvsenna@gmail.com>
2025-07-01 15:48:33 +0530

    QUOTING done

--- src/main.cpp
+++ src/main.cpp
@@ -7,21 +7,30 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  
-
   std::string input;
-  while(1)
+  while (1)
   {
     std::cout << "$ ";
     std::getline(std::cin, input);
 
     auto pos = input.find(' ');
     auto first_token = input.substr(0, pos);
+    if (input[0] == '\'')
+    {
+      pos = input.find('\'', 1);
+      first_token = input.substr(1, pos - 1);
+    }
+    if (input[0] == '"')
+    {
+      pos = input.find('"', 1);
+      first_token = input.substr(1, pos - 1);
+    }
 
     if (first_token == "echo")
     {
@@ -33,8 +42,9 @@ int main() {
       bool in_double_quote = 0;
 
       char prev = 0;
-      for (auto c : line)
+      for (int i = 0; i < line.length(); ++i)
       {
+        char c = line[i];
         if (not in_double_quote and c == '\'')
         {
           single_quote_cnt++;
@@ -47,11 +57,17 @@ int main() {
           in_double_quote = !(double_quote_cnt % 2 == 0);
           continue;
         }
-        if (c == ' ' and prev == ' ' and not (in_single_quote or in_double_quote)) 
+        if (c == ' ' and prev == ' ' and not(in_single_quote or in_double_quote))
         {
-          	continue; 
+          continue;
         }
-
+        if (c == '\\')
+        {
+          if (in_single_quote)
+            putchar(c);
+          
+          c = line[++i];
+        }
         putchar(c);
         prev = c;
       }
@@ -82,19 +98,19 @@ int main() {
       if (command == "my_exe")
       {
         std::string env_path(std::getenv("PATH"));
-        for (const auto word: std::views::split(env_path, ':'))
-        { 
-            auto file_path = std::format("{}/my_exe", std::string_view(word));
-            if (std::filesystem::exists(file_path))
+        for (const auto word : std::views::split(env_path, ':'))
+        {
+          auto file_path = std::format("{}/my_exe", std::string_view(word));
+          if (std::filesystem::exists(file_path))
+          {
+            auto p = std::filesystem::status(file_path).permissions();
+            if ((p & std::filesystem::perms::owner_exec) != std::filesystem::perms::none ||
+                (p & std::filesystem::perms::group_exec) != std::filesystem::perms::none)
             {
-              auto p = std::filesystem::status(file_path).permissions();
-              if ((p & std::filesystem::perms::owner_exec) != std::filesystem::perms::none ||
-                  (p & std::filesystem::perms::group_exec) != std::filesystem::perms::none)
-              {
-                std::cout << command << " is " << file_path << '\n';
-                break;
-              }
+              std::cout << command << " is " << file_path << '\n';
+              break;
             }
+          }
         }
         continue;
       }
@@ -108,46 +124,44 @@ int main() {
 
     if (first_token == "cd")
     {
-        auto target_dir = input.substr(3);
-        if (target_dir  == "~")
-        	target_dir = std::getenv("HOME");
-        
-        if (!std::filesystem::exists(target_dir))
-        {
-            // println("cd: {}: No such file or directory", target_dir);
-            printf("cd: %s: No such file or directory\n", target_dir.c_str());
-            continue;
-        }
+      auto target_dir = input.substr(3);
+      if (target_dir == "~")
+        target_dir = std::getenv("HOME");
 
-        if (chdir(target_dir.c_str()) == 0)
-        { 
-            auto this_path = std::filesystem::current_path().string();
-            if (this_path.starts_with("/private"))
-            {
-                this_path = this_path.substr(8);
-            }
-            setenv("PWD", this_path.c_str(), 1);
-        }
+      if (!std::filesystem::exists(target_dir))
+      {
+        // println("cd: {}: No such file or directory", target_dir);
+        printf("cd: %s: No such file or directory\n", target_dir.c_str());
         continue;
+      }
+
+      if (chdir(target_dir.c_str()) == 0)
+      {
+        auto this_path = std::filesystem::current_path().string();
+        if (this_path.starts_with("/private"))
+        {
+          this_path = this_path.substr(8);
+        }
+        setenv("PWD", this_path.c_str(), 1);
+      }
+      continue;
     }
     if (first_token == "echo" or first_token == "exit" or first_token == "type" or first_token == "pwd")
     {
       std::system(input.c_str());
       continue;
     }
-      
 
-        
     bool bin_exist = false;
     std::string env_path(std::getenv("PATH"));
-    for (const auto word: std::views::split(env_path, ':'))
-    { 
-        auto bin_path = std::format("{}/{}", std::string_view(word), first_token);
-        if (std::filesystem::exists(bin_path))
-        { 
-        	bin_exist = true;
-          break;
-        }
+    for (const auto word : std::views::split(env_path, ':'))
+    {
+      auto bin_path = std::format("{}/{}", std::string_view(word), first_token);
+      if (std::filesystem::exists(bin_path))
+      {
+        bin_exist = true;
+        break;
+      }
     }
     if (bin_exist)
     {
