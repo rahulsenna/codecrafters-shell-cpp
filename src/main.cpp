ed15db577fcd71f8b3122300a2f8f76701b7f693
Rahul Senna <rahulvsenna@gmail.com>
2025-07-03 15:11:54 +0530

    AUTOCOMPLETION done

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,172 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <cstring>
+#include <vector>
+#include <unordered_set>
+
+#define raw_mode 0
+
+#if raw_mode
+#include <termios.h>
+#include <vector>
+#include <unordered_set>
+
+void enable_raw_mode(termios &original)
+{
+  termios raw = original;
+  raw.c_lflag &= ~(ICANON | ECHO); // disable canonical mode and echo
+  tcsetattr(STDIN_FILENO, TCSANOW, &raw);
+}
+void disable_raw_mode(const termios &original)
+{
+  tcsetattr(STDIN_FILENO, TCSANOW, &original); // restore original settings
+}
+#endif
+
+std::vector<std::string> executables = {"echo", "cd", "exit", "pwd"};
+
+void get_execuatables(std::string &env_path)
+{
+  std::unordered_set<std::string> seen(executables.begin(), executables.end()); // Avoid duplicates
+  for (const auto path_range : std::views::split(env_path, ':'))
+  {
+    std::string_view path_sv(path_range.begin(), path_range.end());
+    if (path_sv.empty())
+      continue;
+
+    std::filesystem::path dir_path(path_sv);
+
+    std::error_code ec;
+
+    if (!std::filesystem::exists(dir_path, ec) || !std::filesystem::is_directory(dir_path, ec))
+      continue;
+    for (const auto &entry : std::filesystem::directory_iterator(dir_path, ec))
+    {
+      if (ec)
+        break;
+
+      if (!entry.is_regular_file(ec) || ec)
+        continue;
+
+      const auto &file_path = entry.path();
+
+      auto perms = entry.status(ec).permissions();
+      if (ec)
+        continue;
+
+      // Check for execute permission (owner, group, or others)
+      bool is_executable = (perms & std::filesystem::perms::owner_exec) != std::filesystem::perms::none ||
+                           (perms & std::filesystem::perms::group_exec) != std::filesystem::perms::none ||
+                           (perms & std::filesystem::perms::others_exec) != std::filesystem::perms::none;
+
+      if (is_executable)
+      {
+        std::string filename = file_path.filename().string();
+
+        if (seen.insert(filename).second)
+        {
+          executables.push_back(std::move(filename));
+        }
+      }
+    }
+  }
+}
+
+char *command_generator(const char *text, int state)
+{
+  static int index, len;
+
+  if (state == 0)
+  {
+    index = 0;
+    len = std::strlen(text);
+  }
+
+  for (; index < executables.size(); index++)
+  {
+    if (std::strncmp(executables[index].c_str(), text, len) == 0)
+    {
+      return strdup(executables[index++].c_str());
+    }
+  }
+
+  return nullptr;
+}
+
+char **command_completion(const char *text, int start, int end)
+{
+  if (start == 0)
+  {
+    char **matches = rl_completion_matches(text, command_generator);
+    if (!matches)
+      std::cout << "\x07" << std::flush; // ring the bell
+    else
+    {
+      for (int i = 2; matches[i] != nullptr; i++) // if more than one matches (add one extra space)
+      {
+        auto modified = std::string(matches[i]) + " ";
+        matches[i] = strdup(modified.c_str());;
+      }
+    }
+    return matches;
+  }
+  return nullptr;
+}
+
 int main()
 {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  std::string input;
+  rl_attempted_completion_function = command_completion;
+  std::string env_path(std::getenv("PATH"));
+
+  get_execuatables(env_path);
+
   while (1)
   {
+#if raw_mode
     std::cout << "$ ";
-    std::getline(std::cin, input);
+    
+    termios original;
+    tcgetattr(STDIN_FILENO, &original);
+    enable_raw_mode(original);
+    std::string input;
+    char ch;
+    while (read(STDIN_FILENO, &ch, 1) == 1)
+    {
+      if (ch == '\n')
+        break;
+      
+      if (ch == '\t') 
+      {
+        for (auto &cmd: executables)
+        {
+        	if (cmd.starts_with(input))
+          {
+            input = cmd + " ";
+            std::cout << "\r$ " << input << std::flush;
+          }
+        }
+        continue;
+      }
+      input += ch;
+      std::cout << ch << std::flush;
+    }
+    std::cout << std::endl;
+    disable_raw_mode(original);
+#endif
+
+    char* line = readline("$ ");
+    if (line && *line)
+    {
+        add_history(line);
+    }
+    std::string input(line);
 
     auto pos = input.find(' ');
     auto first_token = input.substr(0, pos);
@@ -175,7 +330,7 @@ int main()
     }
 
     bool bin_exist = false;
-    std::string env_path(std::getenv("PATH"));
+
     for (const auto word : std::views::split(env_path, ':'))
     {
       auto bin_path = std::format("{}/{}", std::string_view(word), first_token);
